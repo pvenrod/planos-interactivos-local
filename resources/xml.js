3f62c614c56660146c08e4c0d11d73e0d88849d6
pvenrod <pvenrod@gmail.com>
2021-06-23 11:02:57 +0200

    Final

---
+++ resources/xml.js
@@ -0,0 +1,114 @@
+<zonas>
+	
+	        <zona>
+		        <id>1</id>
+		        <nombre>Plaza Vieja</nombre>
+		        <imagen>zonas/1.png</imagen>
+		        <imagen_fondo_pagina></imagen_fondo_pagina>
+		        <imagen_plano_actual></imagen_plano_actual>
+		        <imagen_pergamino></imagen_pergamino>
+		
+		        <parcelas>
+			        
+		        </parcelas>
+		
+	        </zona>
+
+	        <zona>
+		        <id>2</id>
+		        <nombre>Puerta Purchena</nombre>
+		        <imagen>zonas/2.png</imagen>
+		        <imagen_fondo_pagina></imagen_fondo_pagina>
+		        <imagen_plano_actual></imagen_plano_actual>
+		        <imagen_pergamino></imagen_pergamino>
+
+		        <parcelas>
+			        <parcela>
+				        <id>14</id>
+				        <imagen>parcelas/14.png</imagen>
+				        <anyo_inicio>1920</anyo_inicio>
+				        <anyo_fin>2020</anyo_fin>
+				        <nombre>Refugios</nombre>
+				        <descripcion>Refugios de Almeria</descripcion>
+				        <multimedias>
+					        <multimedia tipo="">
+						        <titulo></titulo>
+						        <url></url>
+					        </multimedia>
+				        </multimedias>
+			        </parcela>
+			          <parcela>
+				        <id>19<id>
+				        <imagen>parcelas/19.png</imagen>
+				        <anyo_inicio>1940</anyo_inicio>
+				        <anyo_fin>1990</anyo_fin>
+				        <nombre>Casa roja</nombre>
+				        <descripcion>La casa roja de 1940</descripcion>
+				        <multimedias>
+					        <multimedia tipo="">
+						        <titulo></titulo>
+						        <url></url>
+					        </multimedia>
+				        </multimedias>
+			        </parcela>
+			          <parcela>
+				        <id>24</id>
+				        <imagen>parcelas/24.png</imagen>
+				        <anyo_inicio>2000</anyo_inicio>
+				        <anyo_fin>2020</anyo_fin>
+				        <nombre>Casa roja</nombre>
+				        <descripcion>La casa rosa del 2000</descripcion>
+				        <multimedias>
+					        <multimedia tipo="">
+						        <titulo></titulo>
+						        <url></url>
+					        </multimedia>
+				        </multimedias>
+			        </parcela>
+			          <parcela>
+				        <id>27</id>
+				        <imagen>parcelas/27.png</imagen>
+				        <anyo_inicio>1930</anyo_inicio>
+				        <anyo_fin>2020</anyo_fin>
+				        <nombre>Casa amarilla</nombre>
+				        <descripcion>La casa amarilla</descripcion>
+				        <multimedias>
+					        <multimedia tipo="">
+						        <titulo></titulo>
+						        <url></url>
+					        </multimedia>
+				        </multimedias>
+			        </parcela>
+			          <parcela>
+				        <id>28</id>
+				        <imagen>parcelas/28.png</imagen>
+				        <anyo_inicio>1960</anyo_inicio>
+				        <anyo_fin>2020</anyo_fin>
+				        <nombre>Casa azul</nombre>
+				        <descripcion>La casa azul</descripcion>
+				        <multimedias>
+					        <multimedia tipo="">
+						        <titulo></titulo>
+						        <url></url>
+					        </multimedia>
+				        </multimedias>
+			        </parcela>
+			          <parcela>
+				        <id>21</id>
+				        <imagen>parcelas/21.png</imagen>
+				        <anyo_inicio>1900</anyo_inicio>
+				        <anyo_fin>2000</anyo_fin>
+				        <nombre>Casa morada</nombre>
+				        <descripcion>La casa morada</descripcion>
+				        <multimedias>
+					        <multimedia tipo="">
+						        <titulo></titulo>
+						        <url></url>
+					        </multimedia>
+				        </multimedias>
+			        </parcela>
+		        </parcelas>
+
+	        </zona>
+	
+        </zonas>
---
+++ zonas.js
@@ -0,0 +1,457 @@
+var documentoXML =  " \
+<zonas> \
+	<zona> \
+		<id>1</id> \
+		<nombre>Plaza Vieja</nombre> \
+		<imagen>zonas/1.png</imagen> \
+		<imagen_fondo_pagina>parcelas/fondo3.png</imagen_fondo_pagina> \
+		<imagen_plano_actual></imagen_plano_actual> \
+		<imagen_pergamino></imagen_pergamino> \
+		<parcelas> \
+			<parcela> \
+				<id>28</id> \
+				<imagen>parcelas/28.png</imagen> \
+				<anyo_inicio>1820</anyo_inicio> \
+				<anyo_fin>2021</anyo_fin> \
+				<nombre>Casas de alrededor</nombre> \
+				<descripcion>Casas al norte de la plaza vieja</descripcion> \
+				<multimedias> \
+					<multimedia> \
+						<tipo></tipo> \
+						<titulo></titulo> \
+						<url></url> \
+					</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>29</id> \
+				<imagen>parcelas/29.png</imagen> \
+				<anyo_inicio>1820</anyo_inicio> \
+				<anyo_fin>1950</anyo_fin> \
+				<nombre>Cara sur</nombre> \
+				<descripcion>Cara sur de la plaza vieja desde 1820 hasta 1950</descripcion> \
+				<multimedias> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Cara sur en 1845</titulo> \
+						<url>resources/img/multimedia/8.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Cara sur en 1920</titulo> \
+						<url>resources/img/multimedia/14.jpg</url> \
+					</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>30</id> \
+				<imagen>parcelas/30.png</imagen> \
+				<anyo_inicio>1950</anyo_inicio> \
+				<anyo_fin>2021</anyo_fin> \
+				<nombre>Cara sur</nombre> \
+				<descripcion>Cara sur de la plaza vieja desde 1950 hasta la actualidad.</descripcion> \
+				<multimedias> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>31</id> \
+				<imagen>parcelas/31.png</imagen> \
+				<anyo_inicio>1600</anyo_inicio> \
+				<anyo_fin>1920</anyo_fin> \
+				<nombre>Cara este</nombre> \
+				<descripcion>Cara este de la plaza vieja desde 1820 hasta 1920</descripcion> \
+				<multimedias> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Cara este en 1910</titulo> \
+						<url>resources/img/multimedia/7.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>audio</tipo> \
+						<titulo>El Convento de las Claras.</titulo> \
+						<url>resources/img/multimedia/30.mp3</url> \
+					</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>32</id> \
+				<imagen>parcelas/32.png</imagen> \
+				<anyo_inicio>1920</anyo_inicio> \
+				<anyo_fin>2021</anyo_fin> \
+				<nombre>Cara este</nombre> \
+				<descripcion>Cara este de la plaza vieja desde 1920 hasta la actualidad.</descripcion> \
+				<multimedias> \
+					<multimedia> \
+						<tipo>video</tipo> \
+						<titulo>Cara este en 1910</titulo> \
+						<url>resources/img/multimedia/45.mp4</url> \
+					</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>33</id> \
+				<imagen>parcelas/33.png</imagen> \
+				<anyo_inicio>1820</anyo_inicio> \
+				<anyo_fin>1990</anyo_fin> \
+				<nombre>Cara norte</nombre> \
+				<descripcion>Cara norte de la plaza vieja desde 1820 hasta 1990.</descripcion> \
+				<multimedias> \
+					<multimedia> \
+						<tipo>video</tipo> \
+						<titulo>Cara este en 1910</titulo> \
+						<url>resources/img/multimedia/54.mp4</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>audio</tipo> \
+						<titulo>Elementos de la cara Norte.</titulo> \
+						<url>resources/img/multimedia/27.mp3</url> \
+					</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>34</id> \
+				<imagen>parcelas/34.png</imagen> \
+				<anyo_inicio>1990</anyo_inicio> \
+				<anyo_fin>2021</anyo_fin> \
+				<nombre>Cara norte</nombre> \
+				<descripcion>Cara norte de la plaza vieja desde 1990 hasta la actualidad.</descripcion> \
+				<multimedias> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>35</id> \
+				<imagen>parcelas/35.png</imagen> \
+				<anyo_inicio>1820</anyo_inicio> \
+				<anyo_fin>1845</anyo_fin> \
+				<nombre>Cara oeste</nombre> \
+				<descripcion>Cara oeste de la plaza vieja desde 1820 hasta 1845.</descripcion> \
+				<multimedias> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Cara oeste en 1825</titulo> \
+						<url>resources/img/multimedia/8.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Cara oeste en 1830</titulo> \
+						<url>resources/img/multimedia/9.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Cara oeste en 1835</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Interior de la cara oeste en 1835</titulo> \
+						<url>resources/img/multimedia/11.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Cara oeste vista desde otro ángulo</titulo> \
+						<url>resources/img/multimedia/12.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Cara oeste y sur de la Plaza Vieja</titulo> \
+						<url>resources/img/multimedia/13.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>audio</tipo> \
+						<titulo>Ayuntamiento de la ciudad.</titulo> \
+						<url>resources/img/multimedia/26.mp3</url> \
+					</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>36</id> \
+				<imagen>parcelas/36.png</imagen> \
+				<anyo_inicio>1845</anyo_inicio> \
+				<anyo_fin>2021</anyo_fin> \
+				<nombre>Cara oeste</nombre> \
+				<descripcion>Cara oeste de la plaza vieja desde 1845 hasta la actualidad.</descripcion> \
+				<multimedias> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>37</id> \
+				<imagen>parcelas/37.png</imagen> \
+				<anyo_inicio>1920</anyo_inicio> \
+				<anyo_fin>2021</anyo_fin> \
+				<nombre>Cara oeste</nombre> \
+				<descripcion>Zona central de la Plaza Vieja, desde 1920 hasta 2021.</descripcion> \
+				<multimedias> \
+					<multimedia> \
+						<tipo>audio</tipo> \
+						<titulo>Audio explicativo de la Plaza Vieja.</titulo> \
+						<url>resources/img/multimedia/21.mp3</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>audio</tipo> \
+						<titulo>El Pingurucho.</titulo> \
+						<url>resources/img/multimedia/25.mp3</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Pingurucho</titulo> \
+						<url>resources/img/multimedia/pingu.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Arquitectura de la Plaza Vieja</titulo> \
+						<url>resources/img/multimedia/PlazaVieja-alumSciclo.pdf</url> \
+					</multimedia> \
+				</multimedias> \
+			</parcela> \
+		</parcelas> \
+	</zona> \
+	<zona> \
+		<id>2</id> \
+		<nombre>Puerta Purchena</nombre> \
+		<imagen>zonas/2.png</imagen> \
+		<imagen_fondo_pagina>parcelas/fondo3.png</imagen_fondo_pagina> \
+		<imagen_plano_actual></imagen_plano_actual> \
+		<imagen_pergamino></imagen_pergamino> \
+		<parcelas> \
+			<parcela> \
+				<id>14</id> \
+				<imagen>parcelas/14.png</imagen> \
+				<anyo_inicio>1920</anyo_inicio> \
+				<anyo_fin>2020</anyo_fin> \
+				<nombre>Refugios</nombre> \
+				<descripcion>Refugios de Almeria</descripcion> \
+				<multimedias> \
+					<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>imagen</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \<multimedia> \
+						<tipo>documento</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \
+					<multimedia> \
+						<tipo>audio</tipo> \
+						<titulo>Casa vieja</titulo> \
+						<url>resources/img/multimedia/10.jpg</url> \
+					</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>19</id> \
+				<imagen>parcelas/19.png</imagen> \
+				<anyo_inicio>1940</anyo_inicio> \
+				<anyo_fin>1990</anyo_fin> \
+				<nombre>Casa roja</nombre> \
+				<descripcion>La casa roja de 1940</descripcion> \
+				<multimedias> \
+				<multimedia> \
+					<tipo>video</tipo> \
+					<titulo>Casa vieja</titulo> \
+					<url>resources/img/multimedia/10.jpg</url> \
+				</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>24</id> \
+				<imagen>parcelas/24.png</imagen> \
+				<anyo_inicio>2000</anyo_inicio> \
+				<anyo_fin>2020</anyo_fin> \
+				<nombre>Casa roja</nombre> \
+				<descripcion>La casa rosa del 2000</descripcion> \
+				<multimedias> \
+				<multimedia> \
+				<tipo></tipo> \
+				<titulo></titulo> \
+				<url></url> \
+				</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>27</id> \
+				<imagen>parcelas/27.png</imagen> \
+				<anyo_inicio>1930</anyo_inicio> \
+				<anyo_fin>2020</anyo_fin> \
+				<nombre>Casa amarilla</nombre> \
+				<descripcion>La casa amarilla</descripcion> \
+				<multimedias> \
+				<multimedia> \
+				<tipo></tipo> \
+				<titulo></titulo> \
+				<url></url> \
+				</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>28</id> \
+				<imagen>parcelas/28.png</imagen> \
+				<anyo_inicio>1960</anyo_inicio> \
+				<anyo_fin>2020</anyo_fin> \
+				<nombre>Casa azul</nombre> \
+				<descripcion>La casa azul</descripcion> \
+				<multimedias> \
+				<multimedia> \
+				<tipo></tipo> \
+				<titulo></titulo> \
+				<url></url> \
+				</multimedia> \
+				</multimedias> \
+			</parcela> \
+			<parcela> \
+				<id>21</id> \
+				<imagen>parcelas/21.png</imagen> \
+				<anyo_inicio>1900</anyo_inicio> \
+				<anyo_fin>2000</anyo_fin> \
+				<nombre>Casa morada</nombre> \
+				<descripcion>La casa morada</descripcion> \
+				<multimedias> \
+				<multimedia> \
+				<tipo></tipo> \
+				<titulo></titulo> \
+				<url></url> \
+				</multimedia> \
+				</multimedias> \
+			</parcela> \
+		</parcelas> \
+	</zona> \
+	<zona> \
+		<id>3</id> \
+		<nombre>Roquetas</nombre> \
+		<imagen>zonas/roquetas.jpeg</imagen> \
+		<imagen_fondo_pagina>parcelas/roq.png</imagen_fondo_pagina> \
+		<imagen_plano_actual></imagen_plano_actual> \
+		<imagen_pergamino></imagen_pergamino> \
+		<parcelas> \
+		<parcela> \
+				<id>28</id> \
+				<imagen>parcelas/ant.png</imagen> \
+				<anyo_inicio>1900</anyo_inicio> \
+				<anyo_fin>1990</anyo_fin> \
+				<nombre>Zona antigua de Roquetas</nombre> \
+				<descripcion>Conjunto de casas y edificios de Roquetas de Mar, construidos entre 1900 y 1990</descripcion> \
+				<multimedias> \
+				</multimedias> \
+			</parcela> \
+		</parcelas> \
+	</zona> \
+</zonas>"
+
+
+//Audio 21: zona central
+//Audio 25: zona central
+//Audio 26: zona oeste
+//Audio 27: zona norte
+//Audio 30: 
